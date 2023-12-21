7cc368a4124d06d597004f1e9d2ae95d69cbdd39
NGUYỄN THANH HƯƠNG <93818052+hl2802@users.noreply.github.com>
2023-12-21 23:17:39 +0700

    commit1
    commit1

---
+++ Snake/Assets/Scripts/CameraController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Vector3 cameraInitialPosition;
+    private Quaternion cameraInitialRotation;
+    public GameObject mainCamera;
+    public GameObject snakeHead;
+
+    enum CameraPosition { Close, Middle, Far };
+    CameraPosition currentCameraPosition;
+
+    void Start()
+    {
+        InitializeCameraDetails();
+    }
+
+    void InitializeCameraDetails()
+    {
+        currentCameraPosition = CameraPosition.Close;
+
+        cameraInitialPosition = mainCamera.transform.localPosition;
+        cameraInitialRotation = mainCamera.transform.localRotation;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ChangeCamera();
+        }
+    }
+
+    void ChangeCamera()
+    {
+        if (currentCameraPosition == CameraPosition.Close)
+        {
+            SwitchToMiddlePosition();
+        }
+        else if (currentCameraPosition == CameraPosition.Middle)
+        {
+            SwitchToFarPosition();
+        }
+        else if (currentCameraPosition == CameraPosition.Far)
+        {
+            SwitchToClosePosition();
+        }
+    }
+
+    void SwitchToMiddlePosition()
+    {
+        currentCameraPosition = CameraPosition.Middle;
+
+        Transform newTransform = snakeHead.transform;
+        mainCamera.transform.rotation = newTransform.rotation;
+        mainCamera.transform.position = newTransform.position;
+    }
+
+    void SwitchToFarPosition()
+    {
+        currentCameraPosition = CameraPosition.Far;
+
+        mainCamera.transform.localRotation = cameraInitialRotation;
+        mainCamera.transform.localPosition = cameraInitialPosition + new Vector3(0f, 2f, -3f);
+    }
+
+    void SwitchToClosePosition()
+    {
+        currentCameraPosition = CameraPosition.Close;
+
+        mainCamera.transform.localRotation = cameraInitialRotation;
+        mainCamera.transform.localPosition = cameraInitialPosition;
+    }
+}
---
+++ Snake/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+
+    public Text pauseText;
+
+    void Start()
+    {
+        pauseText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        if (Time.timeScale == 1)
+        {
+            // Game is not paused
+            Time.timeScale = 0;             // Pause the game
+            pauseText.gameObject.SetActive(true);
+        }
+        else if (Time.timeScale == 0)
+        {
+            // Game is paused
+            Time.timeScale = 1;             // Continue the game
+            pauseText.gameObject.SetActive(false);
+        }
+    }
+}
